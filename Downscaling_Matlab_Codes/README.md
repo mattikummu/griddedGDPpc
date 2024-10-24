92760470a2a98ceb2543f84c603c4dd722912a56
mkummu@aalto.fi <matti.kummu@aalto.fi>
2024-10-24 08:29:36 +0300

    review R1

--- Downscaling_Matlab_Codes/README.md
+++ Downscaling_Matlab_Codes/README.md
@@ -6,7 +6,7 @@ The following is a brief description of the MATLAB scripts used to downscale GDP
 
 - [Code Usage](#code-usage)
     - [1. Training machine learning models](#1-training-machine-learning-models)
-    - [2. Bias Correction and prediction](#2-bias-correction-and-prediction)
+    - [2. Visulizing errors and predicting adm2 level GDP ratios](#2-visulizing-errors-and-predicting-adm2-level-GDP-ratios)
 - [Dependencies](#dependencies)
 - [Installation](#installation)
 - [Examples](#examples)
@@ -42,30 +42,16 @@ Choose one of the following models to train:
 
 The default option in this script trains all these models. To choose any combination of models, modify the cell variable `models`.
 Default division ratios is 80,0, and 20 percents corresponding to train, validation, and test datasets. Training is done using a 10 fold crossvalidation. Final results are stored in the structured array `Results` with fields corresponding to each machine learnin model listed in `models` variable. Function `trainRegressionModel.m` handles the training process using [Byesian optimizaiton](https://www.mathworks.com/help/stats/bayesianoptimization.html?s_tid=doc_ta) to fine tune the hyperparameters for each model, To predict with the trained model stored in variable `Results` use the following command: `Results.EnsembleBag.mdl.predictFcn(T)`. This command will use `EnsembleBag` model to predict using the data provided in table `T`. `T` must be a table containing data for the features used in the training process.
+After training, the best model will get stored seperately. `best_model` is the name of the best model and the predictor function is stored in `model`. To predict with `model` use the command: `model(T)`. `T` must be a table containing data for the features used in the training process.
+
+### 2. Visulizing errors and predicting adm2 level GDP ratios
+
+The next section first puts cross-validation prediction and test predictions for the best model together in `allVal`. Then adds each prediction to it's corresponding row in the original data and gathers everything in `dataWithPrediction` variable. The rest of this section consists of crating error plots for each admin-year unit for each of the 12 regions and also the histogram of the errors for each region. The plots are saved as a PDF files named `ErrorPlot.pdf` and  `ErrorHist.pdf` respectively. Finally this section produces a csv file named `adm1_prediction_error.csv` which is the adm1 level data with predictions and errors.
+
+Followed by this section is the final prediction for ADM2 level data which is then corrected by ROM and finally saved in `downsclaling_output.csv` file.
 The script ends with saving the workspace to a `mat` file named `TrainedModelsResults.mat`.
-### 2. Bias Correction and Prediction
-
-Trained results are loaded from `TrainedModelsResults.mat`. Here the best model of `EnsembleBag` this script is model independent. The bias correction methods, based on the paper by Belitz and Stackelberg (2021) [^1], are as follows and implemented in the function script `bias_corr.m`.
-* Empirical distribution matching (EDM)
-* Regression of observed on estimated values (ROM)
-* Linear transfer function (LTF)
-* Z-transform
-
-ROM and LTF outputs are linear regression models outputted as functions. Applying these corrections can be done using the following command:
-`corrected_output = predict(MDL, not_corrected_outputs)` where `not_corrected_outputs` are the prediction of the machine learning model and MDL is either ROM and LTF.
-To use EDM the following command must be used:
-`corrected_output = icdf(F_obs, cdf(F_est, not_corrected_outputs))`. 
-To use Z-transform use the following command:
-`corrected_output = mzz * not_corrected_outputs + bzz`
-`F_obs`, `F_est`, `mzz`, and `bzz` are outputs of the `bias_corr.m` function.
-
-Followed by this section is the final prediction for ADM2 level data which is then corrected by ROM and finally saved in `downsclaling_output.csv` file and also `Corrected.mat`.
 
 
 ## Dependencies
 
-This code was developed and tested using MATLAB version 2023b or later. It also requires the Statistics and Machine Learning Toolbox.
-
-
-## References
-[^1]: Belitz, Kenneth, and P. E. Stackelberg. "Evaluation of six methods for correcting bias in estimates from ensemble tree machine learning regression models." Environmental Modelling & Software 139 (2021): 105006. [DOI](https://doi.org/10.1016/j.envsoft.2021.105006).
+This code was developed and tested using MATLAB version 2024a or later. It also requires the Statistics and Machine Learning Toolbox.
--- Downscaling_Matlab_Codes/main.m
+++ Downscaling_Matlab_Codes/main.m
@@ -1,52 +1,149 @@
 clear 
 close all
 clc
+
 %% Loading and cleaning the data
 % Loading raw data
-RawData = readtable('adm1DataForDownscaling.csv');
-% Droping the columns not required in downscaling
-RawData(:,[1,2,4]) = [];
+RawData = readtable('adm1DataForDownscaling.csv'); 
+% Dropping the columns not required for downscaling
+RawData(:,[1,2,4]) = []; 
 
 features = {'gdpRatio', 'adm0GDP', 'adm0gini', 'adm0travelTime', 'adm0urb', 'travelTime', 'urb'};
 targetName = 'gdpRatio';
-% Discarding NA
-fprintf('Before clearing NAN: nRow = %d, nCol = %d \n',size(RawData,1), size(RawData,2))
-idx = zeros(size(RawData));
-for i = 1:size(RawData,2)
-    idx(:,i) = ~isnan(RawData{:,i});
-end
-IDX = all(idx==1,2);
-Data = RawData(IDX,:);
-fprintf('After clearing NAN: nRow = %d, nCol = %d. Data lost = %d \n',...
-    size(Data,1),size(Data,2),abs(size(Data,1)-size(RawData,1)))
+Data = RawData;
+% Printing the size of the data after removing NaN values
+fprintf('After clearing NAN: nRow = %d, nCol = %d. Data lost = %d \n', size(Data,1),size(Data,2),abs(size(Data,1)-size(RawData,1)))
 fprintf('Before outlier clearning: Max= %4f, Min= %4f \n', max(Data.gdpRatio), min(Data.gdpRatio))
 % Identifying and removing the outliers
 Data = rmoutliers(Data,"percentiles",[0.25 99.75],"DataVariables","gdpRatio");
-fprintf('After outlier clearning: Max= %4f, Min= %4f, nRow = %d, nCol = %d. Data lost = %d \n', ...
-    max(Data.gdpRatio), min(Data.gdpRatio), size(Data,1), ...
-    size(Data,2),abs(size(Data,1)-size(RawData,1)))
+% Printing the size of the data and max and min of gdpRatio after outlier removal
+fprintf('After outlier clearning: Max= %4f, Min= %4f, nRow = %d, nCol = %d. Data lost = %d \n', max(Data.gdpRatio), min(Data.gdpRatio), size(Data,1), size(Data,2),abs(size(Data,1)-size(RawData,1)))
 
-%% Training Procress
+%% Training Process
 % Specify the models we want to train:
-% models = {'EnsembleBag', 'EnsembleBoost', 'NN3', 'NN2', 'NN1', 'SVMPly', 'SVMGaussian','LR'};
-models = {'LR'};
-% Dividing the data
-[trainInd, ~, testInd] = dividerand(size(Data,1), 0.8,0,0.2);
+models = {'EnsembleBag','EnsembleBoost', 'NN2', 'SVMGaussian','LR'};
+trainingData = [];
+trainResponse = [];
+testInput = [];
+testResponse = [];
 
-trainingData = Data(trainInd, features);
-trainResponse = Data{trainInd, targetName};
-testInput = Data(testInd, features);
-testResponse = Data{testInd, targetName};
+% Split the data into training and testing data for each RegionID
+for c = 1:12
+    clip = Data(Data.RegionID == c, :);
+    target = clip.gdpRatio;
+    [trainInd, ~, testInd] = dividerand(size(clip,1), 0.8,0,0.2);
+    trainingData = [trainingData; clip(trainInd, :)];
+    trainResponse = [trainResponse; clip{trainInd, targetName}];
+    testInput = [testInput; clip(testInd, :)];
+    testResponse = [testResponse; clip{testInd, targetName}];
+end
 
+% Train the model
 for alg = models
     tic
     Algorithm = alg{1,1};
-    [res.mdl, res.ValAcc, res.valPred, res.OptRes] = ...
-    trainRegressionModel(trainingData, targetName, features(2:end), Algorithm);
+    % Trains a regression model using the training data, features and target
+    [res.mdl, res.ValAcc, res.valPred, ~] = trainRegressionModel(trainingData, targetName, features(2:end), Algorithm);
     Results.(Algorithm) = res;
     tt = toc;
-    fprintf('%s - (Runtime = %f)\n',Algorithm, tt)
-
+    % Print the algorithm used, its runtime and the RMSE
+    fprintf('%s - (Runtime = %f) and the RMSE is: %f\n',Algorithm, tt, res.ValAcc)
 end
 
-% save('TrainedModelsResults.mat')
+% Find the best model
+fields = fieldnames(Results);
+best_model = fields{1}; % Initialize with first field
+best_model_acc = Results.(fields{1}).ValAcc; % Initialize with first ValAcc
+
+for i = 2:length(fields)
+    if Results.(fields{i}).ValAcc < best_model_acc
+        best_model = fields{i};
+        best_model_acc = Results.(fields{i}).ValAcc;
+    end
+end
+
+% Select the model with best accuracy
+model = Results.(best_model).mdl.predictFcn;
+%% Error plots
+% Get cross validation and test predictions together.
+
+allVal = [Results.(best_model).valPred; model(testInput)];
+
+% Find unique regions names
+roi = cell(12,1);
+
+for i = 1:12
+    a = Data.RegName(Data.RegionID == i);
+    roi{i,1} = a{1,1};
+end
+
+% Add corss validation predictions to the data
+for k = 1:size(Data,1)
+    idx = ismember(Data,dataWithPrediction(k,1:11),'rows');
+    dataWithPrediction.prediction(k) = allVal(k);
+end
+
+% Calculate absolute error
+dataWithPrediction.absolute_error = dataWithPrediction.gdpRatio - dataWithPrediction.prediction;
+
+% Plot error for each admin-year unit for each region
+figure
+tiledlayout(4,3)
+for i = 1:12
+    clip = dataWithPrediction(dataWithPrediction.RegionID == i, :);
+    target = clip.gdpRatio;
+    prediction = clip.ens_out;
+    errors=target-prediction;
+    RMSE=sqrt(mean(errors(:).^2));
+    error_mean=mean(errors(:));
+    error_std=std(errors(:));
+    nexttile
+    plot(errors);
+    formated = '%s \n RMSE = %.2f';
+    txt = sprintf(formated, roi{i,1}, RMSE);
+    title(txt);
+
+    ylabel('Predicted - Target')
+     ylim([-1, 3.5])
+end
+
+print('ErrorPlot','-dpdf','-bestfit')
+
+% Plot error histogram for each region
+figure
+tiledlayout(4,3)
+for i = 1:12
+    clip = dataWithPrediction(dataWithPrediction.RegionID == i, :);
+    target = clip.gdpRatio;
+    prediction = clip.ens_out;
+    errors=target-prediction;
+    RMSE=sqrt(mean(errors(:).^2));
+    error_mean=mean(errors(:));
+    error_std=std(errors(:));
+    nexttile
+    h = histogram(errors);
+    h.BinWidth = 0.05;
+    formated = '%s \n Mean = %.2f, STD = %.2f';
+    txt = sprintf(formated, roi{i,1}, error_mean, error_std);
+    title(txt);
+
+    ylabel('Frequency')
+    xlim([-1, 2])
+end
+print('ErrorHist','-dpdf','-bestfit')
+
+writetable(dataWithPrediction, 'adm1_prediction_error.csv')
+
+%% Downscaling prediction
+% Load admin2 level data
+adm2_data = readtable("adm2DataForDownscaling.csv");
+
+% Predict gdpRatio for admin2 level data
+prediction = model(adm2_data);
+
+% Add the predictions to the data
+adm2_data.Prediction = prediction;
+
+% Save the data
+writetable(adm2_data, 'downsclaling_output.csv')
+save('TrainedModelsResults.mat')
